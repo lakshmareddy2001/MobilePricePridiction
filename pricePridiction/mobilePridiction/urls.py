adf3864f6a3449073754c2c7ac846f713e05aea8
lakshmareddy2001 <79352428+lakshmareddy2001@users.noreply.github.com>
2022-07-30 12:42:40 +0530

    Delete pricePridiction directory

--- pricePridiction/mobilePridiction/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
-
-urlpatterns = [
-    path("",views.index,name="index")
-]
